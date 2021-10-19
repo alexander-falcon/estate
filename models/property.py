8228b531ca01c1dcec98d16930fd7923f78a64f0
Alexander Falcon <alexander.falcon.ruiz@gmail.com>
2021-10-19 16:35:49 -0400

    views

--- models/property.py
+++ models/property.py
@@ -6,16 +6,16 @@ class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate property'
 
-    name = fields.Char(required=True)
+    name = fields.Char(string='Title', required=True)
     description = fields.Text()
     active = fields.Boolean(default=True)
     state = fields.Selection(selection=[('new', 'New'), ('received', 'Offer Received'), ('accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')])
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(), 3000))
+    date_availability = fields.Date(string='Available From', copy=False, default=lambda self: fields.Date.today())
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
